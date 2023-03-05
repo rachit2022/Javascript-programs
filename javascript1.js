5e458348008e0447b54cfe62bf9f3d395926ff84
Rachit Gujpta <102291472+rachit2022@users.noreply.github.com>
2023-03-05 18:54:15 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,14 @@
+//console.log("This is a javascript file");
+function greet(name, greetname) {
+    console.log(greetname + " " + name);
+    console.log(name + " is a good boy");
+}
+let names = "Rachit";
+let name1 = "Utsav";
+let name2 = "Raghu";
+let name3 = "Nikesh";
+let greetna = "Good Morning";
+greet(names, greetna);
+greet(name1, greetna);
+greet(name2, greetna);
+greet(name2, greetna);
